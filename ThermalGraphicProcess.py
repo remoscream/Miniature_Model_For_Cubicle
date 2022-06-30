81b7ed178dcd56a26d67ef7ee223f7a4ce504900
remoscream <56284999+remoscream@users.noreply.github.com>
2022-06-30 14:20:46 +0900

    Add comments for ThermalGraphicProcess.py

--- ThermalGraphicProcess.py
+++ ThermalGraphicProcess.py
@@ -1,6 +1,13 @@
-# https://stackoverflow.com/questions/50330251/how-to-merge-mesh-grid-points-from-two-rectangles-in-python
-# https://qiita.com/shu32/items/f19635eab402ea6fc44e
+"""
+Program for resize a 8x8 thermal graphic from AMG8833 by a new resolution
 
+Original program is from:
+https://stackoverflow.com/questions/50330251/how-to-merge-mesh-grid-points-from-two-rectangles-in-python
+https://qiita.com/shu32/items/f19635eab402ea6fc44e
+
+Document for scipy.interpolate:
+https://docs.scipy.org/doc/scipy/reference/interpolate.html
+"""
 
 import numpy as np
 from scipy.interpolate import griddata
@@ -8,53 +15,43 @@ import functions_common as fun
 import matplotlib.pyplot as plt
 
 
+# Set new resolution and method of interpolation
+resolution = 100
+method_interpolation = 'cubic'  # You can use 'linear', 'nearest' and 'cubic'
+
+# Read a data file in NAS(not from sensor)
 filename = '/home/pi/NAS/20220629000339.csv'
 
 data = fun.read_csv_as_list(filename)
 data = np.array(data)
+value = data.flatten()  # Data need to be 1 dimension for scipy.griddata
 
-# Resize data and plot new figure
-# image_array = np.array(sensordata, np.float32)
-# data = cv2.cvtColor(image_array,cv2.COLOR_GRAY2BGR)
-#
-# cv2.imwrite(datafolder + timestamp + '.png', image_array)
-#
-# data_new = cv2.imread(datafolder + timestamp + '.png')
-# print(data_new)
-
-
-# x = np.arange(0,8,1)
-# y = np.arange(0,8,1)
-# X, Y = np.meshgrid(x,y)
-#
-# npts = 100
-# px, py = np.random.choice(x, npts), np.random.choice(y, npts)
-
-x = np.linspace(0,7,8)
-y = np.linspace(0,7,8)
-
+# As AMG8833 only output 8x8 thermal graphic
+# Create points (coordinates) with 8x8 meshgrid
+x = np.linspace(0, 7, 8)
+y = np.linspace(0, 7, 8)
 x, y = np.meshgrid(x, y)
-positions = np.vstack([x.ravel(), y.ravel()])
-theGridPoints = (np.array(positions)).T
+coordinates = np.vstack([x.ravel(), y.ravel()])
+points = (np.array(coordinates)).T
 
+# Create new mesh points with a new resolution
+x_new = np.linspace(0, 7, resolution)
+y_new = np.linspace(0, 7, resolution)
+xx, yy = np.meshgrid(x_new, y_new)
 
-X,Y = np.meshgrid(x,y)
+# Resize thermal graphic from 8x8 to a new resolution
+result = griddata(points, value, (xx, yy), method=method_interpolation)
 
-
-x_new = np.linspace(0,7,1000)
-y_new = np.linspace(0,7,1000)
-
-xx,yy = np.meshgrid(x_new,y_new)
-
-result = griddata(theGridPoints, data.flatten().T, (xx, yy), method='cubic')
-
+# Plot data and save to figure
 plt.subplots(figsize=(8, 4))
 
 plt.subplot(1, 2, 1)
-fig = plt.imshow(data, cmap="inferno")
+plt.imshow(data, cmap="inferno")
 plt.colorbar()
 
 plt.subplot(1, 2, 2)
-fig = plt.imshow(result, cmap="inferno")
+plt.imshow(result, cmap="inferno")
 plt.colorbar()
+
+plt.savefig('/home/pi/NAS/data.png')    # Save figure to NAS
 plt.show()
